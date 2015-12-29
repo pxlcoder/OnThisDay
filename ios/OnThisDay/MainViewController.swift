7bb67e91a9e3e7e907e0f7e9908caef280bac9d6
Aditya Keerthi <pxlcoder@gmail.com>
2015-12-29 18:03:34 -0500

    Began work on iOS app
    Updated .gitignore to reflect additions

---
+++ ios/OnThisDay/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  OnThisDay
+//
+//  Created by Aditya Keerthi on 2015-12-29.
+//  Copyright © 2015 Aditya Keerthi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.title = "On This Day"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios/OnThisDayTests/OnThisDayTests.swift
@@ -0,0 +1,36 @@
+//
+//  OnThisDayTests.swift
+//  OnThisDayTests
+//
+//  Created by Aditya Keerthi on 2015-12-29.
+//  Copyright © 2015 Aditya Keerthi. All rights reserved.
+//
+
+import XCTest
+@testable import OnThisDay
+
+class OnThisDayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
